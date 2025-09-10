ae6c0ba854735e3f06f75826174d9ff5d6d0ec64
Starceva <anzelikastar0806@gmail.com>
2025-09-10 11:30:27 +0300

    gerda poprosila

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 
 public class DragAndDrop : MonoBehaviour,
     IPointerDownHandler,
@@ -10,7 +11,7 @@ public class DragAndDrop : MonoBehaviour,
     private CanvasGroup canvasGro;
     private RectTransform rectTra;
     public ObjectScript objectScr;
-
+    public ScreenBoundariesScript screenBou;
 
     void Start()
     {
@@ -29,12 +30,35 @@ public class DragAndDrop : MonoBehaviour,
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        if(Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+            objectScr.lastDragged = null;
+            canvasGro.blocksRaycasts = false;
+            canvasGro.alpha = 0.6f;
+            rectTra.SetAsLastSibling();
+            Vector3 cursorWorldPos = 
+                Camera.main.ScreenToWorldPoint(new Vector3(
+                    Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z));
+            rectTra.position = cursorWorldPos;
+
+            screenBou.screenPoint = Camera.main.WorldToScreenPoint(rectTra.localPosition);
+
+            screenBou.offset = rectTra.localPosition -
+                 Camera.main.ScreenToWorldPoint(
+                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z));
+            
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        if (Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+            Vector3 curScreenPoint =
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + screenBou.offset;
+            rectTra.position = screenBou.GetClampedPosition(curPosition);
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
---
+++ Assets/Scripts/ScreenBoundariesScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ScreenBoundariesScript : MonoBehaviour
+{
+
+    [HideInInspector]
+    public Vector3 screenPoint, offset;
+    private float minX, maxX, minY, maxY;
+    public float padding = 0.02f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        Vector3 lowerLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        Vector3 upperRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        float widthReduction = (upperRight.x - lowerLeft.x) * padding;
+        float heightReduction = (upperRight.y - lowerLeft.y) * padding;
+
+        minX = lowerLeft.x + widthReduction;
+        maxX = upperRight.x - widthReduction;
+        minY = lowerLeft.y + heightReduction;
+        maxY = upperRight.y - heightReduction;
+    }
+
+    public Vector2 GetClampedPosition(Vector3 position)
+    {
+        return new Vector2(
+            Mathf.Clamp(position.x, minX, maxY),
+            Mathf.Clamp(position.y, minY, maxX)
+       );
+    }
+}
